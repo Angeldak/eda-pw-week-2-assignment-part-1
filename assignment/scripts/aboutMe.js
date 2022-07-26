e9a0f2f551538d348c4bac1932e68938a6b214f0
Lloyd Prock <lloyd.prock@gmail.com>
2022-07-26 15:26:04 -0500

    completed requirements

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -37,17 +37,28 @@ const allowedPets = 4;
 console.log("allowed pets are:", allowedPets);
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
-
+if (adventurous) {
+    console.log("Adventures are great!");
+} else {
+    console.log("How about we stay home?");
+}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
-// 15 - Write a conditional that console logs "I can have more pets!" 
+if (luckyNumber === 2 && adventurous) {
+    console.log("Roll the dice!");
+}
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets) {
+    console.log("I can have more pets!");
+} else if (pets === allowedPets) {
+    console.log("I have enough pets");
+} else if (pets > allowedPets) {  //this could also just be an else, correct?
+    console.log("Oh no, I have too many pets!");
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
